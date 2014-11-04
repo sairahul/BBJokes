ce63b7b7a740040d6ec78435c42fa1dfafb13478
Sai Rahul <sairahul@gmail.com>
2014-11-03 21:10:52 -0500

    removed unnecessary files

--- platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes.dex : \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes/com/bb/jokes/BuildConfig.class \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes/com/bb/jokes/CordovaApp.class \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes/com/bb/jokes/R$attr.class \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes/com/bb/jokes/R$drawable.class \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes/com/bb/jokes/R$string.class \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes/com/bb/jokes/R$xml.class \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/classes/com/bb/jokes/R.class \
-/Users/SaiRahul/myfiles/BBJokes/platforms/android/ant-build/dexedLibs/classes-c8310aa46de08f9cc69ee0009503a250.jar \
